4a4e599b167e8df151f846d38a1d756bb108dc58
unknown <ajithramsri@gmail.com>
2021-07-13 09:58:27 -0700

    second-Release

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MainApplication from './components/MainApplication';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document. title = "Data Quest"
+    }, [])
   return (
     <div className="App">
       <MainApplication></MainApplication>
